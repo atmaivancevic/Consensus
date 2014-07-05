d3d4a32b4cc5d738f261392cc3918d9f433ff548
Atma <atma.ivancevic@adelaide.edu.au>
2014-07-05 15:39:13 +0930

    make consensus for the 2nd_Clustering alignments

--- make_consensus.sh
+++ make_consensus.sh
@@ -7,7 +7,7 @@
 
 # Go to directory containing the alignments for each cluster, for this species
 # (both original and Gblocks-improved alignments)
-cd /scratch/atmaGenomes/GenomeAnalysis/Fulllength_L1s/Clusters/$1
+cd /scratch/atmaGenomes/GenomeAnalysis/Fulllength_L1s/2nd_Clustering/Clusters/$1/
 
 # Make a consensus sequence from each Gblocks-improved alignment file
 for i in *_gbHalf.afa; #currently set to make consensus out of gbHalf alignments
--- run_consensus_on_all.sh
+++ run_consensus_on_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #L1s
-arr=( 'Mouse' 'Human' 'Marmoset' 'CrabeatingMacaque' 'PrairieVole' 'Rat' 'GreenMonkey' 'Goat' )
+arr=( 'Aardvark' 'Alpaca' 'Armadillo' 'Baboon' 'Baiji' 'BigBrownBat' 'BlackFlyingFox' 'Bonobo' 'Bushbaby' 'CapeElephantShrew' 'CapeGoldenMole' 'Cat' 'Chimp' 'Chinchilla' 'ChineseHamster' 'ChineseTreeShrew' 'CommonShrew' 'Cow' 'CrabeatingMacaque' 'DeerMouse' 'Dog' 'Dolphin' 'Elephant' 'Ferret' 'Gibbon' 'Goat' 'GoldenHamster' 'Gorilla' 'GreaterHorseshoeBat' 'GreenMonkey' 'GuineaPig' 'Hedgehog' 'Horse' 'Human' 'KangarooRat' 'KillerWhale' 'LesserEgyptianJerboa' 'Manatee' 'Marmoset' 'Megabat' 'Microbat' 'MinkeWhale' 'Mouse' 'MouseLemur' 'NakedMolerat' 'Opossum' 'SumatranOrangutan' 'Pig' 'PrairieVole' 'Rabbit' 'Rat' 'Rhesus' 'RockHyrax' 'Sheep' 'SiberianTiger' 'Sloth' 'Squirrel' 'SquirrelMonkey' 'TasDevil' 'Tenrec' 'TibetanAntelope' 'TreeShrew' 'Wallaby' 'Walrus' 'WeddellSeal' 'WhiteRhino' 'Yak' 'Tarsier' 'WaterBuffalo' 'MouseearedBat' 'GreaterFalseVampireBat' 'ParnellsMustachedBat' 'BrandtsBat' )
 
 for j in "${arr[@]}"
 do
